5294b1afdc390e433419f2bcfe0ba8b6dbd14bbc
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-07-31 20:08:31 +0700

    feat: add image import functionality to LatPulldownMachineSeeder and include multiple exercise images

--- database/seeders/LatPulldownMachineSeeder.php
+++ database/seeders/LatPulldownMachineSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Exercise;
 use App\Models\ExerciseCategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
 class LatPulldownMachineSeeder extends Seeder
 {
@@ -141,7 +143,18 @@ class LatPulldownMachineSeeder extends Seeder
             ],
         ];
 
-        foreach ($exercises as $data) {
+        $sourceDir = public_path('exercises/lat-pulldown-machine');
+        $files = glob($sourceDir . '/*.png');
+        sort($files);
+
+        foreach ($exercises as $i => $data) {
+            $sourceFile = $files[$i] ?? null;
+
+            if ($sourceFile) {
+                $imagePath = Storage::disk('public')->putFile('exercises', new File($sourceFile));
+                $data['image'] = $imagePath;
+            }
+
             $categoryId = $categories[$data['category_slug']] ?? null;
             unset($data['category_slug']);
             $data['category_id'] = $categoryId;
