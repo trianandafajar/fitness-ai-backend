a7f20770132a29e1045d7a66f9e78b907c6b2e45
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-07-31 15:49:06 +0700

    feat: add image handling to ShoulderPressMachineSeeder and include multiple exercise images

--- database/seeders/ShoulderPressMachineSeeder.php
+++ database/seeders/ShoulderPressMachineSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Exercise;
 use App\Models\ExerciseCategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 
 class ShoulderPressMachineSeeder extends Seeder
 {
@@ -120,7 +122,18 @@ class ShoulderPressMachineSeeder extends Seeder
             ],
         ];
 
-        foreach ($exercises as $data) {
+        $sourceDir = public_path('execises/shoulder-press-machine');
+        $files = glob($sourceDir . '/*.png');
+        sort($files);
+
+        foreach ($exercises as $i => $data) {
+            $sourceFile = $files[$i] ?? null;
+
+            if ($sourceFile) {
+                $imagePath = Storage::disk('public')->putFile('exercises', new File($sourceFile));
+                $data['image'] = $imagePath;
+            }
+
             $categoryId = $categories[$data['category_slug']] ?? null;
             unset($data['category_slug']);
             $data['category_id'] = $categoryId;
