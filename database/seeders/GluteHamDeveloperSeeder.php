7138e5eddfbda8a8540b22a69b8fca86638bdedd
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-08-01 11:28:53 +0700

    feat: add image import functionality and multiple images for Glute Ham Developer

--- database/seeders/GluteHamDeveloperSeeder.php
+++ database/seeders/GluteHamDeveloperSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Exercise;
 use App\Models\ExerciseCategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 
 class GluteHamDeveloperSeeder extends Seeder
 {
@@ -183,7 +185,18 @@ class GluteHamDeveloperSeeder extends Seeder
             ],
         ];
 
-        foreach ($exercises as $data) {
+        $sourceDir = public_path('exercises/glute-ham-developer');
+        $files = glob($sourceDir . '/*.png');
+        sort($files);
+
+        foreach ($exercises as $i => $data) {
+            $sourceFile = $files[$i] ?? null;
+
+            if ($sourceFile) {
+                $imagePath = Storage::disk('public')->putFile('exercises', new File($sourceFile));
+                $data['image'] = $imagePath;
+            }
+
             $categoryId = $categories[$data['category_slug']] ?? null;
             unset($data['category_slug']);
             $data['category_id'] = $categoryId;
